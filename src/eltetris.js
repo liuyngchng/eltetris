904364cb9c350fb2bbf25a7b9468cec5564dd50c
richard <richard@test.com>
2021-09-29 17:03:10 +0800

    format

--- src/eltetris.js
+++ src/eltetris.js
@@ -29,30 +29,30 @@
  *  number_of_rows - Number of rows in the tetris game.
  */
 function ElTetris(number_of_columns, number_of_rows) {
-  this.number_of_rows = number_of_rows;
-  this.number_of_columns = number_of_columns;
-  this.rows_completed = 0;
+    this.number_of_rows = number_of_rows;
+    this.number_of_columns = number_of_columns;
+    this.rows_completed = 0;
 
-  // The board is represented as an array of integers, one integer for each row.
-  this.board = new Array(number_of_rows);
-  for (var i = 0; i < number_of_rows; i++) {
-    this.board[i] = 0;
-  }
+    // The board is represented as an array of integers, one integer for each row.
+    this.board = new Array(number_of_rows);
+    for (var i = 0; i < number_of_rows; i++) {
+        this.board[i] = 0;
+    }
 
-  this.FULLROW = Math.pow(2, number_of_columns) - 1;
+    this.FULLROW = Math.pow(2, number_of_columns) - 1;
 }
 
-ElTetris.prototype.play = function() {
-  var piece = this.getRandomPiece();
-  var move = this.pickMove(piece);
- 
-  var last_move = this.playMove(this.board, move.orientation, move.column);
+ElTetris.prototype.play = function () {
+    var piece = this.getRandomPiece();
+    var move = this.pickMove(piece);
 
-  if (!last_move.game_over) {
-    this.rows_completed += last_move.rows_removed;
-  }
+    var last_move = this.playMove(this.board, move.orientation, move.column);
 
-  return last_move;
+    if (!last_move.game_over) {
+        this.rows_completed += last_move.rows_removed;
+    }
+
+    return last_move;
 };
 
 /**
@@ -70,38 +70,38 @@ ElTetris.prototype.play = function() {
  *     * orientation - The orientation of the piece to use.
  *     * column - The column at which to place the piece.
  */
-ElTetris.prototype.pickMove = function(piece) {
-  var best_evaluation = -100000;
-  var best_orientation = 0;
-  var best_column = 0;
-  var evaluation = undefined;
+ElTetris.prototype.pickMove = function (piece) {
+    var best_evaluation = -100000;
+    var best_orientation = 0;
+    var best_column = 0;
+    var evaluation = undefined;
 
-  // Evaluate all possible orientations
-  for (var i in piece) {
-    var orientation = piece[i].orientation;
+    // Evaluate all possible orientations
+    for (var i in piece) {
+        var orientation = piece[i].orientation;
 
-    // Evaluate all possible columns
-    for (var j = 0; j < this.number_of_columns - piece[i].width + 1; j++) {
-      // Copy current board
-      var board = this.board.slice();
-      var last_move = this.playMove(board, orientation, j);
+        // Evaluate all possible columns
+        for (var j = 0; j < this.number_of_columns - piece[i].width + 1; j++) {
+            // Copy current board
+            var board = this.board.slice();
+            var last_move = this.playMove(board, orientation, j);
 
-      if (!last_move.game_over) {
-        evaluation = this.evaluateBoard(last_move, board);
+            if (!last_move.game_over) {
+                evaluation = this.evaluateBoard(last_move, board);
 
-        if (evaluation > best_evaluation) {
-          best_evaluation = evaluation;
-          best_orientation = i;
-          best_column = j;
+                if (evaluation > best_evaluation) {
+                    best_evaluation = evaluation;
+                    best_orientation = i;
+                    best_column = j;
+                }
+            }
         }
-      }
     }
-  }
 
-  return {
-    'orientation': piece[best_orientation].orientation,
-    'column': best_column
-  };
+    return {
+        'orientation': piece[best_orientation].orientation,
+        'column': best_column
+    };
 };
 
 /**
@@ -118,13 +118,13 @@ ElTetris.prototype.pickMove = function(piece) {
  *   A number indicating how "good" a board is, the higher the number, the
  *   better the board.
  */
-ElTetris.prototype.evaluateBoard = function(last_move, board) {
-  return GetLandingHeight(last_move, board) * -4.500158825082766 +
-      last_move.rows_removed * 3.4181268101392694 +
-      GetRowTransitions(board, this.number_of_columns) * -3.2178882868487753 +
-      GetColumnTransitions(board, this.number_of_columns) * -9.348695305445199 +
-      GetNumberOfHoles(board, this.number_of_columns) * -7.899265427351652 +
-      GetWellSums(board, this.number_of_columns) * -3.3855972247263626;
+ElTetris.prototype.evaluateBoard = function (last_move, board) {
+    return GetLandingHeight(last_move, board) * -4.500158825082766 +
+        last_move.rows_removed * 3.4181268101392694 +
+        GetRowTransitions(board, this.number_of_columns) * -3.2178882868487753 +
+        GetColumnTransitions(board, this.number_of_columns) * -9.348695305445199 +
+        GetNumberOfHoles(board, this.number_of_columns) * -7.899265427351652 +
+        GetWellSums(board, this.number_of_columns) * -3.3855972247263626;
 };
 
 /**
@@ -138,75 +138,75 @@ ElTetris.prototype.evaluateBoard = function(last_move, board) {
  * Returns:
  *   True if play succeeded, False if game is over.
  */
-ElTetris.prototype.playMove = function(board, piece, column) {
-  piece = this.movePiece(piece, column);
-  var placementRow = this.getPlacementRow(board, piece);
-  var rowsRemoved = 0;
+ElTetris.prototype.playMove = function (board, piece, column) {
+    piece = this.movePiece(piece, column);
+    var placementRow = this.getPlacementRow(board, piece);
+    var rowsRemoved = 0;
 
-  if (placementRow + piece.length > this.number_of_rows) {
-    // Game over.
-    return { 'game_over' : true };
-  }
-
-  // Add piece to board.
-  for (var i = 0; i < piece.length; i++) {
-    board[placementRow + i] |= piece[i];
-  }
-
-  // Remove any full rows
-  for (i = 0; i < piece.length; i++) {
-    if (board[placementRow + i] == this.FULLROW) {
-      board.splice(placementRow + i, 1);
-      // Add an empty row on top.
-      board.push(0);
-      // Since we have decreased the number of rows by one, we need to adjust
-      // the index accordingly.
-      i--;
-      rowsRemoved++;
+    if (placementRow + piece.length > this.number_of_rows) {
+        // Game over.
+        return {'game_over': true};
     }
-  }
 
-  return {
-    'landing_height' : placementRow,
-    'piece' : piece,
-    'rows_removed' : rowsRemoved,
-    'game_over' : false
-  };
+    // Add piece to board.
+    for (var i = 0; i < piece.length; i++) {
+        board[placementRow + i] |= piece[i];
+    }
+
+    // Remove any full rows
+    for (i = 0; i < piece.length; i++) {
+        if (board[placementRow + i] == this.FULLROW) {
+            board.splice(placementRow + i, 1);
+            // Add an empty row on top.
+            board.push(0);
+            // Since we have decreased the number of rows by one, we need to adjust
+            // the index accordingly.
+            i--;
+            rowsRemoved++;
+        }
+    }
+
+    return {
+        'landing_height': placementRow,
+        'piece': piece,
+        'rows_removed': rowsRemoved,
+        'game_over': false
+    };
 };
 
 /**
  * Given a piece, return the row at which it should be placed.
  */
-ElTetris.prototype.getPlacementRow = function(board, piece) {
-  // Descend from top to find the highest row that will collide
-  // with the our piece.
-  for (var row = this.number_of_rows - piece.length; row >= 0; row--) {
-    // Check if piece collides with the cells of the current row.
-    for (var i = 0; i < piece.length; i++) {
-      if ((board[row + i] & piece[i]) !== 0) {
-        // Found collision - place piece on row above.
-        return row + 1;
-      }
+ElTetris.prototype.getPlacementRow = function (board, piece) {
+    // Descend from top to find the highest row that will collide
+    // with the our piece.
+    for (var row = this.number_of_rows - piece.length; row >= 0; row--) {
+        // Check if piece collides with the cells of the current row.
+        for (var i = 0; i < piece.length; i++) {
+            if ((board[row + i] & piece[i]) !== 0) {
+                // Found collision - place piece on row above.
+                return row + 1;
+            }
+        }
     }
-  }
 
-  return 0; // No collision found, piece should be placed on first row.
+    return 0; // No collision found, piece should be placed on first row.
 };
 
-ElTetris.prototype.movePiece = function(piece, column) {
-  // Make a new copy of the piece
-  var newPiece = piece.slice();
-  for (var i = 0; i < piece.length; i++) {
-    newPiece[i] = piece[i] << column;
-  }
+ElTetris.prototype.movePiece = function (piece, column) {
+    // Make a new copy of the piece
+    var newPiece = piece.slice();
+    for (var i = 0; i < piece.length; i++) {
+        newPiece[i] = piece[i] << column;
+    }
 
-  return newPiece;
+    return newPiece;
 };
 
-ElTetris.prototype.getRandomPieceIndex = function() {
-  return Math.floor(Math.random() * PIECES.length);
+ElTetris.prototype.getRandomPieceIndex = function () {
+    return Math.floor(Math.random() * PIECES.length);
 };
 
-ElTetris.prototype.getRandomPiece = function() {
-  return PIECES[this.getRandomPieceIndex()];
+ElTetris.prototype.getRandomPiece = function () {
+    return PIECES[this.getRandomPieceIndex()];
 };
--- src/features.js
+++ src/features.js
@@ -24,7 +24,7 @@
  */
 
 function GetLandingHeight(last_move, board) {
-  return last_move.landing_height + ((last_move.piece.length - 1) / 2);
+    return last_move.landing_height + ((last_move.piece.length - 1) / 2);
 }
 
 /**
@@ -33,28 +33,28 @@ function GetLandingHeight(last_move, board) {
  * on the same row and vice versa.
  */
 function GetRowTransitions(board, num_columns) {
-  var transitions = 0;
-  var last_bit = 1;
+    var transitions = 0;
+    var last_bit = 1;
 
-  for (var i = 0; i < board.length; ++i) {
-    var row = board[i];
+    for (var i = 0; i < board.length; ++i) {
+        var row = board[i];
 
-    for (var j = 0; j < num_columns; ++j) {
-      var bit = (row >> j) & 1;
-      
-      if (bit != last_bit) {
-        ++transitions;
-      }
+        for (var j = 0; j < num_columns; ++j) {
+            var bit = (row >> j) & 1;
 
-      last_bit = bit;
+            if (bit != last_bit) {
+                ++transitions;
+            }
+
+            last_bit = bit;
+        }
+
+        if (bit == 0) {
+            ++transitions;
+        }
+        last_bit = 1;
     }
-
-    if (bit == 0) {
-      ++transitions;
-    }
-    last_bit = 1;
-  }
-  return transitions;
+    return transitions;
 }
 
 /**
@@ -63,43 +63,43 @@ function GetRowTransitions(board, num_columns) {
  * on the same row and vice versa.
  */
 function GetColumnTransitions(board, num_columns) {
-  var transitions = 0;
-  var last_bit = 1;
+    var transitions = 0;
+    var last_bit = 1;
 
-  for (var i = 0; i < num_columns; ++i) {
-    for (var j = 0; j < board.length; ++j) {
-      var row = board[j];
-      var bit = (row >> i) & 1;
-      
-      if (bit != last_bit) {
-        ++transitions;
-      }
+    for (var i = 0; i < num_columns; ++i) {
+        for (var j = 0; j < board.length; ++j) {
+            var row = board[j];
+            var bit = (row >> i) & 1;
 
-      last_bit = bit;
+            if (bit != last_bit) {
+                ++transitions;
+            }
+
+            last_bit = bit;
+        }
+
+        last_bit = 1;
     }
 
-    last_bit = 1;
-  }
-  
-  return transitions;
+    return transitions;
 }
 
 function GetNumberOfHoles(board, num_columns) {
-  var holes = 0;
-  var row_holes = 0x0000;
-  var previous_row = board[board.length - 1];
+    var holes = 0;
+    var row_holes = 0x0000;
+    var previous_row = board[board.length - 1];
 
-  for (var i = board.length - 2; i >= 0; --i) {
-    row_holes = ~board[i] & (previous_row | row_holes);
+    for (var i = board.length - 2; i >= 0; --i) {
+        row_holes = ~board[i] & (previous_row | row_holes);
 
-    for (var j = 0; j < num_columns; ++j) {
-      holes += ((row_holes >> j) & 1);
+        for (var j = 0; j < num_columns; ++j) {
+            holes += ((row_holes >> j) & 1);
+        }
+
+        previous_row = board[i];
     }
 
-    previous_row = board[i];
-  }
-
-  return holes;
+    return holes;
 }
 
 /**
@@ -117,64 +117,64 @@ function GetNumberOfHoles(board, num_columns) {
  *    1 + 2 + 3 + ... + n. This gives more significance to deeper holes.
  */
 function GetWellSums(board, num_columns) {
-  var well_sums = 0;
+    var well_sums = 0;
 
-  // Check for well cells in the "inner columns" of the board.
-  // "Inner columns" are the columns that aren't touching the edge of the board.
-  for (var i = 1; i < num_columns - 1; ++i) {
+    // Check for well cells in the "inner columns" of the board.
+    // "Inner columns" are the columns that aren't touching the edge of the board.
+    for (var i = 1; i < num_columns - 1; ++i) {
+        for (var j = board.length - 1; j >= 0; --j) {
+            if ((((board[j] >> i) & 1) == 0) &&
+                (((board[j] >> (i - 1)) & 1) == 1) &&
+                (((board[j] >> (i + 1)) & 1) == 1)) {
+
+                // Found well cell, count it + the number of empty cells below it.
+                ++well_sums;
+
+                for (var k = j - 1; k >= 0; --k) {
+                    if (((board[k] >> i) & 1) == 0) {
+                        ++well_sums;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    // Check for well cells in the leftmost column of the board.
     for (var j = board.length - 1; j >= 0; --j) {
-      if ((((board[j] >> i) & 1) == 0) && 
-          (((board[j] >> (i - 1)) & 1) == 1) &&
-          (((board[j] >> (i + 1)) & 1) == 1)) {
+        if ((((board[j] >> 0) & 1) == 0) &&
+            (((board[j] >> (0 + 1)) & 1) == 1)) {
 
-        // Found well cell, count it + the number of empty cells below it.
-        ++well_sums;
-
-        for (var k = j - 1; k >= 0; --k) {
-          if (((board[k] >> i) & 1) == 0) {
+            // Found well cell, count it + the number of empty cells below it.
             ++well_sums;
-          } else {
-            break;
-          }
+
+            for (var k = j - 1; k >= 0; --k) {
+                if (((board[k] >> 0) & 1) == 0) {
+                    ++well_sums;
+                } else {
+                    break;
+                }
+            }
         }
-      }
     }
-  }
 
-  // Check for well cells in the leftmost column of the board.
-  for (var j = board.length - 1; j >= 0; --j) {
-    if ((((board[j] >> 0) & 1) == 0) && 
-      (((board[j] >> (0 + 1)) & 1) == 1)) {
+    // Check for well cells in the rightmost column of the board.
+    for (var j = board.length - 1; j >= 0; --j) {
+        if ((((board[j] >> (num_columns - 1)) & 1) == 0) &&
+            (((board[j] >> (num_columns - 2)) & 1) == 1)) {
+            // Found well cell, count it + the number of empty cells below it.
 
-      // Found well cell, count it + the number of empty cells below it.
-      ++well_sums;
-
-      for (var k = j - 1; k >= 0; --k) {
-        if (((board[k] >> 0) & 1) == 0) {
-          ++well_sums;
-        } else {
-          break;
+            ++well_sums;
+            for (var k = j - 1; k >= 0; --k) {
+                if (((board[k] >> (num_columns - 1)) & 1) == 0) {
+                    ++well_sums;
+                } else {
+                    break;
+                }
+            }
         }
-      }
     }
-  }
 
-  // Check for well cells in the rightmost column of the board.
-  for (var j = board.length - 1; j >= 0; --j) {
-    if ((((board[j] >> (num_columns - 1)) & 1) == 0) && 
-      (((board[j] >> (num_columns - 2)) & 1) == 1)) {
-      // Found well cell, count it + the number of empty cells below it.
-
-      ++well_sums;
-      for (var k = j - 1; k >= 0; --k) {
-        if (((board[k] >> (num_columns - 1)) & 1) == 0) {
-          ++well_sums;
-        } else {
-          break;
-        }
-      }
-    }
-  }
-
-  return well_sums;
+    return well_sums;
 }
